d50d1dfe6c5db1c2b90e6bb5fb721c672486a56e
Madray Haven <sgpublic2002@gmail.com>
2022-11-16 16:23:20 +0800

    init project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import org.gradle.internal.impldep.org.bouncycastle.cms.RecipientId.password
+
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("org.jetbrains.intellij") version "1.9.0"
 }
 
 group = "io.github.sgpublic"
@@ -14,8 +16,8 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
-    type.set("IC") // Target IDE Platform
+    version.set("2022.2.3")
+    type.set("AI") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
 }
@@ -23,16 +25,16 @@ intellij {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     patchPluginXml {
         sinceBuild.set("213")
-        untilBuild.set("223.*")
+        untilBuild.set("222.*")
     }
 
     signPlugin {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "ExSharedPreference"
+rootProject.name = "ExspPlugin"
